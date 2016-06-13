18c9f1dcf68464ed8c6043aa20fb5a12d1e77285
Lydia <lmweeden.12@gmail.com>
2016-06-13 16:48:59 -0700

    Initializing 001_Lydia_Input_Calculator

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     $message_count = $message_count + $random_message_count;
     $output_message = "Hello, {$name}. You have {$message_count} messages!";
     echo $output_message;
+//initializing 001_Lydia_Input_Calculator
 
 
 ?>
